4cbd3f8a74cd9de69f8f5f668b7036831ccae7e6
Zack Powers <zpowers@etshost.com>
2012-04-02 09:37:29 -0400

    Create a protocol for XMLRPCEncoderImpl to implement.
    This protocol will be used instead of XMLRPCEncoderImpl directly.

--- XMLRPCEncoder.h
+++ XMLRPCEncoder.h
@@ -1,11 +1,14 @@
+//
+//  XMlRPCEncoder.h
+//  XMLRPC
+//
+//  Created by Zack Powers on 4/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
-#import "XMLRPCEncoder.h"
 
-@interface XMLRPCEncoderImpl : NSObject <XMLRPCEncoder> {
-    NSString *myMethod;
-    NSArray *myParameters;
-}
-
+@protocol XMLRPCEncoder <NSObject>
 - (NSString *)encode;
 
 #pragma mark -
@@ -13,7 +16,6 @@
 - (void)setMethod: (NSString *)method withParameters: (NSArray *)parameters;
 
 - (void)setParameters: (NSArray*)parameters;
-
 #pragma mark -
 
 - (NSString *)method;

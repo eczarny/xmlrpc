499158ef2ce5db9a00009e9f676afa6af07ab921
Zack Powers <zpowers@etshost.com>
2012-04-04 13:58:14 -0400

    Remove unnecessary prototypes from XMLRPCDefaultEncoder
    The methods in defined in XMLRPCDefaultEncoder.h are already defined in the
    XMLRPCEncoder protocol and there are are redundant.

--- XMLRPCDefaultEncoder.h
+++ XMLRPCDefaultEncoder.h
@@ -5,17 +5,4 @@
     NSString *myMethod;
     NSArray *myParameters;
 }
-
-- (NSString *)encode;
-
-#pragma mark -
-
-- (void)setMethod: (NSString *)method withParameters: (NSArray *)parameters;
-
-#pragma mark -
-
-- (NSString *)method;
-
-- (NSArray *)parameters;
-
 @end
